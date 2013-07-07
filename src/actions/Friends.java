854f2329b43fbf4f3c29db0d48b8ef750dd3f935
DavidAQ <153019747@qq.com>
2013-07-07 14:50:40 +0800

    libraries

--- src/actions/Friends.java
+++ src/actions/Friends.java
@@ -17,8 +17,8 @@ public class Friends extends BaseAction {
     public String list() {
         FriendItem[] list = new FriendItem[] {
             new FriendItem(10123,"David","static/images/bulb.png","watch",true),
-            new FriendItem(10124,"AQ","static/images/pi.png","watch",false),
-            new FriendItem(10125,"FG","static/images/picmi.png","watch",false),
+            new FriendItem(10124,"AQ","static/images/pi.png","friend",false),
+            new FriendItem(10125,"FG","static/images/picmi.png","friend",false),
             new FriendItem(10127,"HWB","static/images/logo.png","watch",true)
         };
         return jsonResult(list);
--- src/actions/I18n.java
+++ src/actions/I18n.java
@@ -1,14 +1,19 @@
 
 package actions;
 
+import java.util.HashMap;
+
 public class I18n extends BaseAction {
     
     public static class RequestLang {
         public String lang;
     }
+    
     public String dictionary() {
         RequestLang lang = (RequestLang) getParam(RequestLang.class);
-        return jsonResult(lang);
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("sitename", "驴圈");
+        return jsonResult(result);
     }
     
 }
