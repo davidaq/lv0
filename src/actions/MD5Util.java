fa3661ecb4ce7d17062ed04af1e4c4e227880d77
fgwork <fgwork@172.28.14.108>
2013-07-09 20:02:43 +0800

    md5

--- src/actions/MD5Util.java
+++ src/actions/MD5Util.java
@@ -1,29 +1,39 @@
 package actions;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+ 
 public class MD5Util {
-    public final static String MD5(String s) {
-        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+ 
+    /**
+     *
+     * @param plainText
+     *            明文
+     * @return 32位密文
+     */
+    public static String MD5(String plainText) {
+        String re_md5 = new String();
         try {
-            byte[] btInput = s.getBytes();
-            // 获得MD5摘要算法的 MessageDigest 对象
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
-            // 使用指定的字节更新摘要
-            mdInst.update(btInput);
-            // 获得密文
-            byte[] md = mdInst.digest();
-            // 把密文转换成十六进制的字符串形式
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(plainText.getBytes());
+            byte b[] = md.digest();
+ 
+            int i;
+ 
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
             }
-            return new String(str);
-        } catch (Exception e) {
+ 
+            re_md5 = buf.toString();
+ 
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
-            return null;
         }
+        return re_md5;
     }
 }
