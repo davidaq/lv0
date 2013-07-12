29e65832e135b0e74d80e0879df123dec2be1465
fgwork <fgwork@172.28.14.108>
2013-07-12 16:26:00 +0800

    Plan

--- src/actions/Plan.java
+++ src/actions/Plan.java
@@ -1,12 +1,14 @@
 package actions;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import dao.PlanDao;
 import dao.PlanitemDao;
 import dao.ResortDao;
 
 
+import sun.net.www.content.text.plain;
 import tables.Planitem;
 import tables.Resort;
 import tables.Userinfo;
@@ -89,9 +91,126 @@ public class Plan extends BaseAction {
 		
 		p.setPlanHeadline(param.planHeadline);
 		p.setPlanContent(param.planContent);
-//		if(param.planFavor)
-//		p.setPlanFavor(1);
+		if(param.planFavor){
+			Byte b = 1;
+			p.setPlanFavor(b);
+		}
+		else {
+			Byte b = 0;
+			p.setPlanFavor(b);
+		}
 		pd.updatePlan(p);
 		return jsonResult("");
 	}
+	
+	
+	public static class AddPlanItemParam{
+		Planitem planItem;
+	}
+	
+	public String addPlanItem(){
+		AddPlanItemParam param = (AddPlanItemParam) getParam(AddPlanItemParam.class);
+		if(param.planItem == null){
+			return jsonResult("planItem");
+		}
+		if(param.planItem.getPlanId() == null){
+			return jsonResult("planItem");
+		}
+		if(param.planItem.getStartdate() == null || param.planItem.getStartdate().equals("")){
+			return jsonResult("startDate");
+		}
+		if(param.planItem.getEnddate() == null || param.planItem.getEnddate().equals("")){
+			return jsonResult("endDate");
+		}
+		param.planItem.setPlanItemId(null);
+		PlanDao pd = new PlanDao();
+		tables.Plan p = pd.findPlanbyid(param.planItem.getPlanId());
+		
+		if(p == null){
+			return jsonResult("planId");
+		}
+		PlanitemDao pid = new PlanitemDao();
+		pid.addPlanitem(param.planItem);
+		
+		return jsonResult("ok");
+	}
+	
+	
+	public static class EditPlanItemParam{
+		int planItemId;
+		int resortId;
+		String resName;
+		Date startDate;
+		Date endDate;
+	}
+	
+	public String editPlanItem(){
+		EditPlanItemParam param = (EditPlanItemParam) getParam(AddPlanItemParam.class);
+		if(param.startDate == null){
+			return jsonResult("startDate");
+		}
+		if(param.endDate == null){
+			return jsonResult("endDate");
+		}
+		PlanitemDao pid = new PlanitemDao();
+		Planitem p = pid.findPlanitembyid(param.planItemId);
+		if(p == null){
+			return jsonResult("planItemId");
+		}
+		
+		ResortDao rd = new ResortDao();
+		Resort r = rd.findResortById(param.resortId);
+		if(r == null){
+			p.setResortId(null);
+		}
+		
+		p.setResName(param.resName);
+		p.setStartdate(param.startDate);
+		p.setEnddate(param.endDate);
+		pid.updatePlanitem(p);
+		
+		return jsonResult("ok");
+	}
+	
+	
+	
+	public static class DeletePlanParam{
+		int planId;
+	}
+	
+	public String deletePlan(){
+		DeletePlanParam param = (DeletePlanParam) getParam(DeletePlanParam.class);
+		PlanDao pd = new PlanDao();
+		tables.Plan p = pd.findPlanbyid(param.planId);
+		if(p == null){
+			return jsonResult("planId");
+		}
+		PlanitemDao pid = new PlanitemDao();
+		ArrayList<Planitem> piList = pid.findPlanitemDaoByplanId(param.planId);
+		if(piList != null && piList.size() > 0){
+			for(Planitem planitem : piList){
+				pid.deletePlanitem(planitem);
+			}
+		}
+		pd.deletePlan(p);		
+		return jsonResult("ok");
+	}
+	
+	
+	
+	public static class DeletePlanitemParam{
+		int planitemId;
+	}
+	
+	public String deletePlanitem(){
+		DeletePlanitemParam param = (DeletePlanitemParam) getParam(DeletePlanitemParam.class);
+		PlanitemDao pid = new PlanitemDao();
+		Planitem pi = pid.findPlanitembyid(param.planitemId);
+		if(pi == null){
+			return jsonResult("planitemId");
+		}
+		pid.deletePlanitem(pi);
+		return jsonResult("ok");
+	}
+
 }
